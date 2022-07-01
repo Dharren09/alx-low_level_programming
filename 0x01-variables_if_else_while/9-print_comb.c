6842129bdceaad4c69f6e403dffee1a9227f528d
Dharren09 <dharrydharrenzerah@gmail.com>
2022-07-01 14:44:27 -0700

    Done

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- *main - print 0-9 separated with commas, using putchar
+ *main - print 0-9 separated with commas using putchar
  *Return: Always 0 (Success)
  */
 
@@ -21,6 +21,5 @@ int main(void)
 	}
 	putchar('\n');
 
-	retun (0);
+	return (0);
 }
-
