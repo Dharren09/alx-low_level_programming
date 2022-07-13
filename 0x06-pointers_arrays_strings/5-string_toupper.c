b09a6c09381d2ba2faf783fdb5e2c8425371c5e6
Dharren09 <dharrydharrenzerah@gmail.com>
2022-07-13 12:33:51 -0700

    Done

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * string_toupper - C hanges all lowercase letters to upper case
+ * @p: The string will be modified
+ *
+ * Return: Char var
+ */
+char *string_toupper(char *p)
+{
+	int a = 0;
+
+	while (p[a])
+	{
+		if (p[a] >= 97 && p[a] <= 122)
+		{
+			p[a] -= 32;
+		}
+
+		a++;
+	}
+	 return (p);
+}
