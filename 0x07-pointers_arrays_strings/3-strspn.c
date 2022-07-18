8e688d56802189fcb0ef5505f17ff08cebcb5583
Dharren09 <dharrydharrenzerah@gmail.com>
2022-07-18 12:21:33 -0700

    Done

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
 /**
- * * _strspn - gets the length of a prefix substring.
+ * *_strspn - gets the length of a prefix substring.
  *
  * @s: string.
  * @accept: bytes.
@@ -10,7 +10,7 @@
 unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int i, j;
-	
+
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; accept[j] != s[i]; j++)
