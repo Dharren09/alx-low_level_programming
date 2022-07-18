515b0a197389d68f2a7c51c436fb42a9e3fdccbc
Dharren09 <dharrydharrenzerah@gmail.com>
2022-07-18 14:32:02 -0700

    Done

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,40 +1,28 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * *_strpbrk - searches a string for any of a set of bytes
+ * *_strpbrk - locates the first occurrence of string s in string
+ * accept
  *
- * @s: string
- * @accept: bytes
- * Return: pointer to the byte in s
+ * @s: string scanned
+ * @c: character to be located
+ * @accept: test
+ * Return: pointer ttto byte in s
  */
-char *_strbrk(char *s, char *accept)
+char *_strpbrk(char *s, char *accept)
 {
-	if (s == NULL || accept == NULL)
-	{
-		return (NULL);
-	}
+	int i;
 
 	while (*s)
 	{
-		if (_strchr(accept, *s))
-			return (s);
+		for (i = 0; accept[i]; i++)
+		{
+			if (*s == accept[i])
+			{
+				return (s);
+			}
+		}
 		s++;
 	}
-	return (NULL);
-}
-/**
- * *_strchr - locates a character in a string
- *
- * @s: string
- * @c: chracter to be located
- * Return: pointer to the first occuence of NULL if no character found
- */
-char *_strchr(char *s, char c)
-{
-	do {
-		if (*s == c)
-		       return (s);
-	} while (*s++);
-	return (NULL);
+	return (0);
 }
