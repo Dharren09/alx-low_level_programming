9f938ad5b0d72729f9c2b9fd1593ec0dd0e1d9e7
Dharren09 <dharrydharrenzerah@gmail.com>
2022-07-22 07:04:57 -0700

    Done

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,33 +1,16 @@
 #include "main.h"
 #include <stdio.h>
 
-void print_string(char *str);
-
 /**
  * main - Prints its name
- * @argc: length of the argv
- * @argv: Arrays of strings of the program arguments
- * Return: 0, success
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: Always zero
  */
-int main(int argc __attribute__((unused)), char *argv[])
+int main(int argc, char *argv[])
 {
-	print_string(argv[0]);
-	putchar('\n');
+	(void) argc;
+	printf("%s\n", argv[0]);
 
 	return (0);
-}
-
-/** print_string - Prints all char of a string
- * @str: Pointer to string
- * Return: void
- */
-void prin_string(char *str)
-{
-	int i = 0;
-
-	while (str[i] != '\0')
-	{
-		putchar(str[i]);
-		i++;
-	}
 }
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,6 +1,6 @@
 #ifndef main_h
 #define main_h
 
-int main(int argc __attribute__((unused)), char *argv[])
+int main(int argc, char *argv[]);
 
 #endif
