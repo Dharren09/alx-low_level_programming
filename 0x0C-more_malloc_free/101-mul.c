c33c7d0f700a0e835eccbfe97a84320d84a2494d
Dharren09 <dharrydharrenzerah@gmail.com>
2022-07-29 10:11:24 -0700

    Done

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,4 +1,6 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - multiplies two positive numbers
@@ -14,14 +16,17 @@ int main(int argc, char *argv[])
 	if (argc != 3)
 	{
 		printf("Error\n");
-		exit(98)
+		exit(98);
 	}
-	for (i = 1, i < argc; i++)
+	for (i = 1; i < argc; i++)
 	{
-		if (argv[i][j] > 57 || aargv[i][j] < 48)
+		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			printf("Error\n");
-			exit(98);
+			if (argv[i][j] > 57 || argv[i][j] < 48)
+			{
+				printf("Error\n");
+				exit(98);
+			}
 		}
 	}
 	mul = atol(argv[1]) * atol(argv[2]);
