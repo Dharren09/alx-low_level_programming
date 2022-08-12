c8cf08f51e0c6b05e6bfad43df65a96c3ea3bd33
Dharren09 <dharrydharrenzerah@gmail.com>
2022-08-12 13:09:12 -0700

    Done

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -4,8 +4,6 @@
 #include "strlen.c"
 #include <stdlib.h>
 
-list_t *createNewNode(const char *str);
-
 /**
  * add_node_end - dds a new node at the end of a list_t list
  * @head: douple pointer to the head of the linked list
@@ -15,55 +13,32 @@ list_t *createNewNode(const char *str);
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *current_node = *head;
+	list_t *new_list, *temp;
 
-	if (current_node)
+	if (str != NULL)
 	{
-		while (current_node)
+		new_list = malloc(sizeof(list_t));
+		if (new_list == NULL)
+			return (NULL);
+		new_list->str = strdup(str);
+		new_list->len = _strlen(str);
+		new_list->next = NULL;
+
+		if (*head == NULL)
 		{
-			if (current_node->next)
-			{
-				current_node = current_node->next;
-			}
-			else
-			{
-				current_node->next = createNewNode(str);
-				return (current_node->next);
-			}
+			*head = new_list;
+			return (*head);
+		}
+		else
+		{
+			temp = *head;
+			while (temp->next)
+				temp = temp->next;
+
+			temp->next = new_list;
+			return (temp);
 		}
 	}
-	else
-	{
-		*head = createNewNode(str);
-	}
 
-		return (*head);
-}
-
-/**
- * createNewNode - create a new list_t list node
- * @str: pointer to string to be assigned to the created node's str property
- * Return: pointer to the new node (SUCCESS) OR
- * NULL, if there is insufficent memory available (FAILURE)
- */
-
-list_t *createNewNode(const char *str)
-{
-	list_t *new_node_ptr = malloc(sizeof(list_t));
-
-	if (new_node_ptr)
-		return (NULL);
-
-	new_node_ptr->str = strdup(str);
-
-	if (!(new_node_ptr->str))
-	{
-		free(new_node_ptr);
-		return (NULL);
-	}
-
-	new_node_ptr->len = _strlen(new_node_ptr->str);
-	new_node_ptr->next = NULL;
-
-	return (new_node_ptr);
+	return (NULL);
 }
