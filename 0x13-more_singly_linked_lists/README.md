3808d0f990c78b5e8b3cb87972cf4d7a6b9e8b60
Dharren09 <dharrydharrenzerah@gmail.com>
2022-08-16 04:11:42 -0700

    Done

--- 0x13-more_singly_linked_lists/README.md
+++ 0x13-more_singly_linked_lists/README.md
@@ -1,1 +1,1 @@
-Free list
+Pop
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,7 +1,6 @@
 #ifndef LISTS_H
 #define LISTS_H
 #include <stddef.h>
-
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -15,6 +14,16 @@ typedef struct listint_s
 	int n;
 	struct listint_s *next;
 } listint_t;
+/**
+ * struct listadd_s - singly linked list
+ * @address: Address of a node in a list
+ * @next: Pointer to next node
+ */
+typedef struct listadd_s
+{
+	void *address;
+	struct listadd_s *next;
+} listadd_t;
 
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
@@ -31,5 +40,7 @@ listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
+listadd_t *add_nodeaddress(listadd_t **head, const listint_t *address);
+void free_listadd(listadd_t *head);
 
 #endif /* lists.h */
