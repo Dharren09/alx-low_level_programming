e22e3ee3ba9b43e1a424d8cbc10acab91677188f
Dharren09 <dharrydharrenzerah@gmail.com>
2022-08-19 08:59:41 -0700

    Done

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -42,7 +42,7 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 	{
 		while (nlen > 0)
 		{
-			if ((m& 1) != (n & 1))
+			if ((m & 1) != (n & 1))
 				flip += 1;
 			m = m >> 1;
 			n = n >> 1;
